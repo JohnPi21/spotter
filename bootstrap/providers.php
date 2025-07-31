ac067eff5c0697f002df3795bee666a7294e0ab0
Alex John <imalex96ro@gmail.com>
2025-07-31 14:22:17 +0300

    Telescope and predis installed for dev

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,6 +1,12 @@
 <?php
 
-return [
+$providers = [
     App\Providers\AppServiceProvider::class,
     App\Providers\EventServiceProvider::class,
 ];
+
+if ($app->environment('local', 'development')) {
+    $providers[] = App\Providers\TelescopeServiceProvider::class;
+}
+
+return $providers;
