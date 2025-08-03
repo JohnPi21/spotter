01f08224848ee4689caa989e40cad9b948282f06
Alex John <imalex96ro@gmail.com>
2025-08-04 02:58:52 +0300

    Revert user factory to using faker

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,12 +24,12 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            // 'name'      => fake()->name(),
-            // 'email'     => fake()->unique()->safeEmail(),
-            // 'email_verified_at' => now(),
-            // 'password' => static::$password ??= Hash::make('password'),
-            // 'remember_token' => Str::random(10),
-            // 'created_at'    => fake()->dateTimeBetween('-4 weeks', '-1 week'),
+            'name'      => fake()->name(),
+            'email'     => fake()->unique()->safeEmail(),
+            'email_verified_at' => now(),
+            'password' => static::$password ??= Hash::make('password'),
+            'remember_token' => Str::random(10),
+            'created_at'    => fake()->dateTimeBetween('-4 weeks', '-1 week'),
         ];
     }
 
