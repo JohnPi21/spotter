03bce7b4071f5d7ae2fa03a694a62bc0ef1d4ee1
Alex John <imalex96ro@gmail.com>
2025-08-18 17:02:00 +0300

    Fixing tests permissions seeding

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,20 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Spatie\Permission\PermissionRegistrar;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->seed();
+
+        $this->app->make(PermissionRegistrar::class)->forgetCachedPermissions();
+    }
 }
